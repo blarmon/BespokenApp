9b809a1846134e569e96edd96040df53e5005e59
Lauren Naylor <lauren.naylor@drake.edu>
2014-10-25 11:11:47 -0500

    added pull to refresh for poem pages

--- src/com/example/bespokenapp/Poem.java
+++ src/com/example/bespokenapp/Poem.java
@@ -5,8 +5,10 @@
 package com.example.bespokenapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
@@ -14,16 +16,33 @@ import android.webkit.WebViewClient;
 
 public class Poem extends Activity {
 
+	SwipeRefreshLayout swipeView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_poem);
-		
-		String address = getIntent().getExtras().getString("url");
-		WebView myWebView;
+
+		final String address = getIntent().getExtras().getString("url");
+		final WebView myWebView;
 		myWebView = (WebView) findViewById(R.id.poemWebView);
 		myWebView.loadUrl(address);
 		myWebView.setWebViewClient(new WebViewClient()); 
+
+		swipeView = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);	 
+		swipeView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				swipeView.setRefreshing(true);
+				myWebView.loadUrl(address);
+				( new Handler()).postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						swipeView.setRefreshing(false);
+					}
+				}, 3000);
+			}
+		});
 
 	}
 
@@ -44,8 +63,10 @@ public class Poem extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 	public void trashClicked(){
 		//here
 	}
 }
+
+
