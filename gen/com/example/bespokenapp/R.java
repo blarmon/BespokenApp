92f5ec1fe77000daa5f0e0b7e12ab3ef73044bae
Alex Peterson <petestreet01@gmail.com>
2014-10-28 21:34:23 -0500

    added custom logo

--- gen/com/example/bespokenapp/R.java
+++ gen/com/example/bespokenapp/R.java
@@ -23,13 +23,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int bespokenicon=0x7f020000;
-        public static final int delete81=0x7f020001;
-        public static final int homeedited=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int loginbackground=0x7f020004;
-        public static final int male66=0x7f020005;
-        public static final int microphone21=0x7f020006;
-        public static final int search7=0x7f020007;
+        public static final int bespokenlogo=0x7f020001;
+        public static final int delete81=0x7f020002;
+        public static final int homeedited=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int loginbackground=0x7f020005;
+        public static final int male66=0x7f020006;
+        public static final int microphone21=0x7f020007;
+        public static final int search7=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f08001e;
